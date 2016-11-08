02519ec4bd9dd00bd13eb9d7d0ecb7f2fe97a78f
Kristoffer Rath Hansen <kristoffer@codingpirates.dk>
2016-11-09 00:51:03 +0100

    Add logo

--- index.php
+++ index.php
@@ -2,7 +2,7 @@
 include("header.php");
 ?>
 <div class="row">
-  <div class="col-md-4"></div>
+  <div class="col-md-4"><img src="logo.png" class="img-responsive" title"Coding Pirates logo" width="100" height="100" /></div>
   <div class="col-md-4">Dette er Coding Pirates Teaminator til vores Gamejam 2016!</div>
   <div class="col-md-4"></div>
 </div>

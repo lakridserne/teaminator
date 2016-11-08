ca13c52132c55e1e74ed358f82f540197037abdc
Kristoffer Rath Hansen <kristoffer@codingpirates.dk>
2016-11-09 00:39:13 +0100

    Add some layout

--- header.php
+++ header.php
@@ -3,7 +3,7 @@ $teaminator_url = "https://www.rathhansen.com/teaminator/";
 ?>
 <!DOCTYPE html>
 <head>
-  <title>Coding Pirates Team-inator</title>
+  <title>Coding Pirates Teaminator</title>
   <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/css/bootstrap.min.css" integrity="sha384-BVYiiSIFeK1dGmJRAkycuHAHRg32OmUcww7on3RYdg4Va+PmSTsz/K68vbdEjh4u" crossorigin="anonymous">
   <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/css/bootstrap-theme.min.css" integrity="sha384-rHyoN1iRsVXV4nD0JutlnGaslCJuC7uwjduW9SVrLvRYooPp2bWYgmgJQIXwl/Sp" crossorigin="anonymous">
   <script src="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/js/bootstrap.min.js" integrity="sha384-Tc5IQib027qvyjSMfHjOMaLkfuWVxZxUPnCJA7l2mCWNIpG9mGCD8wGNIcPD7Txa" crossorigin="anonymous"></script>
@@ -11,7 +11,7 @@ $teaminator_url = "https://www.rathhansen.com/teaminator/";
 <nav class="navbar navbar-default">
   <div class="container-fluid">
     <div class="navbar-header">
-      <a class="navbar-brand" href="<?php echo $teaminator_url; ?>" title="Coding Pirates Teaminator">Coding Pirates Team-inator til Coding Pirates Game Jam 2016!</a>
+      <a class="navbar-brand" href="<?php echo $teaminator_url; ?>" title="Coding Pirates Teaminator">Coding Pirates Teaminator til Coding Pirates Gamejam 2016!</a>
     </div>
     <ul class="nav navbar-nav">
 
--- index.php
+++ index.php
@@ -1,5 +1,11 @@
 <?php
 include("header.php");
-echo $_SERVER['PHP_SELF'];
+?>
+<div class="row">
+  <div class="col-md-4"></div>
+  <div class="col-md-4">Dette er Coding Pirates Teaminator til vores Gamejam 2016!</div>
+  <div class="col-md-4"></div>
+</div>
+<?php
 include("footer.php");
 ?>

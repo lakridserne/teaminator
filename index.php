f875810cc907a30588dfa6708ee3137ed0f694b4
Kristoffer Rath Hansen <kristoffer@codingpirates.dk>
2016-11-09 01:12:07 +0100

    Add further explanation

--- index.php
+++ index.php
@@ -3,7 +3,11 @@ include("header.php");
 ?>
 <div class="row">
   <div class="col-md-4"><img src="logo.png" class="img-responsive" title"Coding Pirates logo" width="100" height="100" /></div>
-  <div class="col-md-4">Dette er Coding Pirates Teaminator til vores Gamejam 2016!</div>
+  <div class="col-md-4">
+    Dette er Coding Pirates Teaminator til vores Gamejam 2016!
+
+    Klik på fanerne ovenfor for at bruge systemet.
+  </div>
   <div class="col-md-4"></div>
 </div>
 <?php

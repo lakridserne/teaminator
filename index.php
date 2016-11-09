ecc7a3435fb4d4d10c59a1dc3853748f579707fa
Kristoffer Rath Hansen <kristoffer@codingpirates.dk>
2016-11-09 01:13:06 +0100

    Make sure it's on separate lines

--- index.php
+++ index.php
@@ -4,9 +4,9 @@ include("header.php");
 <div class="row">
   <div class="col-md-4"><img src="logo.png" class="img-responsive" title"Coding Pirates logo" width="100" height="100" /></div>
   <div class="col-md-4">
-    Dette er Coding Pirates Teaminator til vores Gamejam 2016!
+    <p>Dette er Coding Pirates Teaminator til vores Gamejam 2016!</p>
 
-    Klik på fanerne ovenfor for at bruge systemet.
+    <p>Klik på fanerne ovenfor for at bruge systemet.</p>
   </div>
   <div class="col-md-4"></div>
 </div>

faf2b2c2df711353e9353c528e3aaf1f66821090
Kristoffer Rath Hansen <kristoffer@codingpirates.dk>
2016-11-10 14:50:53 +0100

    Get started on CSV form

--- csv-upload.php
+++ csv-upload.php
@@ -4,12 +4,20 @@
 * Used to generate teams at Coding Pirates Game Jam 2015-2016
 */
 
+include("header.php");
+
 if(!isset($_REQUEST['submit'])) {
-  include("header.php");
-?>
-<p>Upload CSV fil med medlemmer.</p>
-<?php
+  ?>
+  <h2>Upload CSV fil med medlemmer.</h2>
+  <form enctype="multipart/form-data" action="<?php echo $_SERVER['PHP_SELF']; ?>" method="post">
+    <input type="hidden" name="MAX_FILE_SIZE" value="51200" />
+    Vælg filen eller træk den hen på: <input type="file" name="csv_file" />
+    <input type="submit" name="submit" value="Send filen" />
+  </form>
+  <?php
 } else {
   // Code to use CSV file.
+
 }
+include("footer.php");
 ?>

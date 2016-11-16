3513bc172408ff956e05139b74302134e638aa5b
Kristoffer Rath Hansen <kristoffer@codingpirates.dk>
2016-11-16 01:54:15 +0100

    Print out more

--- csv-upload.php
+++ csv-upload.php
@@ -72,8 +72,19 @@ if(!isset($_REQUEST['submit'])) {
   }
 
   // Now we have the file scrubbed - read and put in DB
-  $filehandle = fopen($new_file_location,'r');
-  print_r(fgetcsv($filehandle,0,';'));
+  //$filehandle = fopen($new_file_location,'r');
+  $row = 1;
+  if(($filehandle = fopen($new_file_location, 'r')) !== false) {
+    while(($data = fgetcsv($filehandle,1000,';')) !== false) {
+      $num = count($data);
+      echo "<p> $num fields in line $row: <br /></p>\n";
+      $row++;
+      for ($c=0; $c < $num; $c++) {
+        echo $data[$c] . "<br />\n";
+      }
+    }
+    fclose($filehandle);
+  }
 }
 include("footer.php");
 ?>

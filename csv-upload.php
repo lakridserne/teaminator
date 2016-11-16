b40cd62501ace1476e33778d5f63b412445fab5c
Kristoffer Rath Hansen <kristoffer@codingpirates.dk>
2016-11-16 01:56:48 +0100

    Yep

--- csv-upload.php
+++ csv-upload.php
@@ -76,12 +76,7 @@ if(!isset($_REQUEST['submit'])) {
   $row = 1;
   if(($filehandle = fopen($new_file_location, 'r')) !== false) {
     while(($data = fgetcsv($filehandle,1000,';')) !== false) {
-      $num = count($data);
-      echo "<p> $num fields in line $row: <br /></p>\n";
-      $row++;
-      for ($c=0; $c < $num; $c++) {
-        echo $data[$c] . "<br />\n";
-      }
+      print_r($data);
     }
     fclose($filehandle);
   }

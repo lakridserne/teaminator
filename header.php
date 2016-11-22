8d0bd81ce69e21dd4afe6dc87533726da33c34c1
Kristoffer Rath Hansen <kristoffer@codingpirates.dk>
2016-11-22 23:05:51 +0100

    Dual list box should work now

--- header.php
+++ header.php
@@ -17,6 +17,7 @@ function echoActiveClassIfRequestMatches($requestUri)
   <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/css/bootstrap.min.css" integrity="sha384-BVYiiSIFeK1dGmJRAkycuHAHRg32OmUcww7on3RYdg4Va+PmSTsz/K68vbdEjh4u" crossorigin="anonymous">
   <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/css/bootstrap-theme.min.css" integrity="sha384-rHyoN1iRsVXV4nD0JutlnGaslCJuC7uwjduW9SVrLvRYooPp2bWYgmgJQIXwl/Sp" crossorigin="anonymous">
   <script src="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/js/bootstrap.min.js" integrity="sha384-Tc5IQib027qvyjSMfHjOMaLkfuWVxZxUPnCJA7l2mCWNIpG9mGCD8wGNIcPD7Txa" crossorigin="anonymous"></script>
+  <script src="http://ajax.googleapis.com/ajax/libs/jquery/2.0.0/jquery.min.js"></script>
   <script src="<?php echo $teaminator_url . 'duallistbox/dist/jquery.bootstrap-duallistbox.min.js'; ?>"></script>
   <link rel="stylesheet" type="text/css" href="<?php echo $teaminator_url . 'duallistbox/src/bootstrap-duallistbox.css'; ?>">
 </head>

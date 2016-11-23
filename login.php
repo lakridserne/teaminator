515c636b1c6a62696960929f7f7c633b4f286917
Kristoffer Rath Hansen <kristoffer@codingpirates.dk>
2016-11-23 02:07:28 +0100

    Additional login logic

--- login.php
+++ login.php
@@ -17,7 +17,7 @@ if(!isset($_REQUEST['submit'])) {
         <input type="text" name="username" id="username" />
       </div>
       <div class="form-group">
-        <label for="username">Kodeord</label>
+        <label for="password">Kodeord</label>
         <input type="password" name="password" id="password" />
       </div>
       <br />
@@ -30,6 +30,14 @@ if(!isset($_REQUEST['submit'])) {
 <?php
 } else {
   // login logic
+  if(isset($_REQUEST['username']) && isset($_REQUEST['password'])) {
+    // both user and pass set - let's check it!
+    $login_sql = "SELECT ID, password, salt FROM users WHERE username=:username";
+    $login_val = [[":username",$_REQUEST['username']]];
+
+    $pwd = $db->query($login_sql,$login_val);
+    print_r($pwd);
+  }
 }
 include("footer.php");
 ?>

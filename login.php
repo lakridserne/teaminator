d59c7dfb838c21870ac7f300af246931fb86d0aa
Kristoffer Rath Hansen <kristoffer@codingpirates.dk>
2016-11-23 01:19:09 +0100

    Make login form

--- login.php
+++ login.php
@@ -5,8 +5,27 @@
 */
 
 include("header.php");
+?>
 
+<div class="row">
+  <div class="col-md-4"></div>
+  <div class="col-md-4">
+    <form name="loginForm" action="<?php echo $_SERVER['PHP_SELF']; ?>" method="post">
+      <div class="form-group">
+        <label for="username">Brugernavn</label>
+        <input type="text" name="username" id="username" />
+      </div>
+      <div class="form-group">
+        <label for="username">Kodeord</label>
+        <input type="text" name="password" id="password" />
+      </div>
+      <br />
+      <button name="submit" type="submit" class="btn btn-default btn-block">Log ind</button>
+    </form>
+  </div>
+  <div class="col-md-4"></div>
+</div>
 
-
+<?php
 include("footer.php");
 ?>

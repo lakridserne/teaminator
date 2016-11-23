4f43cb6e0427a7d350c865e12a41241c3ed8ad1c
Kristoffer Rath Hansen <kristoffer@codingpirates.dk>
2016-11-24 00:53:34 +0100

    Rearrange so you can work more efficiently

--- choose.php
+++ choose.php
@@ -6,39 +6,7 @@
 
 include("header.php");
 
-if(!isset($_REQUEST['submit'])) {
-  // Form not submitted
-  // get data
-  $fetch_names_sql = "SELECT ID, name FROM participants WHERE teaminated=0 AND updated_since_csv=0";
-  $names = $db->query($fetch_names_sql);
-  ?>
-  <form action="<?php echo $_SERVER['PHP_SELF']; ?>" method="post" class="names">
-    <select name="names_select" class="names_select">
-      <?php
-        foreach($names as $name) {
-          echo "<option value='" . $name['ID'] . "'>" . $name['name'] . "</option>";
-        }
-      ?>
-    </select>
-    <br />
-    <label for="visualprog">Visuel programmering</label>
-    <input type="checkbox" name="visualprog" value="visualprog" /><br />
-    <label for="textprog">Tekstprogrammering</label>
-    <input type="checkbox" name="textprog" value="textprog" /><br />
-    <label for="graphic">Grafik / game design</label>
-    <input type="checkbox" name="graphic" value="graphic" /><br />
-    <label for="ultra">DR Ultra må gerne følge mig</label>
-    <input type="checkbox" name="ultra" value="ultra" />
-
-    <button type="submit" name="submit" class="btn btn-default btn-block">Opdater interesser</button>
-  </form>
-  <script>
-    $(document).ready(function() {
-      $(".names_select").select2();
-    });
-  </script>
-  <?php
-} else {
+if(isset($_REQUEST['submit'])) {
   // now find the data
   if(isset($_REQUEST['visualprog'])) {
     $visualprog = true;
@@ -69,7 +37,38 @@ if(!isset($_REQUEST['submit'])) {
     [":ID",$_REQUEST['names_select']]
   ];
   $db->query($update_sql,$values);
+  echo "Opdateret<br />";
 }
 
+$fetch_names_sql = "SELECT ID, name FROM participants WHERE teaminated=0 AND updated_since_csv=0";
+$names = $db->query($fetch_names_sql);
+?>
+<form action="<?php echo $_SERVER['PHP_SELF']; ?>" method="post" class="names">
+  <select name="names_select" class="names_select">
+    <?php
+      foreach($names as $name) {
+        echo "<option value='" . $name['ID'] . "'>" . $name['name'] . "</option>";
+      }
+    ?>
+  </select>
+  <br />
+  <label for="visualprog">Visuel programmering</label>
+  <input type="checkbox" name="visualprog" value="visualprog" /><br />
+  <label for="textprog">Tekstprogrammering</label>
+  <input type="checkbox" name="textprog" value="textprog" /><br />
+  <label for="graphic">Grafik / game design</label>
+  <input type="checkbox" name="graphic" value="graphic" /><br />
+  <label for="ultra">DR Ultra må gerne følge mig</label>
+  <input type="checkbox" name="ultra" value="ultra" />
+
+  <button type="submit" name="submit" class="btn btn-default btn-block">Opdater interesser</button>
+</form>
+<script>
+  $(document).ready(function() {
+    $(".names_select").select2();
+  });
+</script>
+
+<?php
 include("footer.php");
 ?>

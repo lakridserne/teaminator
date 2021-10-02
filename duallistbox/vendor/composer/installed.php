9e39f2494b037d138b97a5ae492b2a0a82a53d7b
Kristoffer Rath Hansen <kristoffer@rathhansen.com>
2021-10-03 00:01:32 +0200

    Update more

---
+++ duallistbox/vendor/composer/installed.php
@@ -0,0 +1,24 @@
+<?php return array (
+  'root' => 
+  array (
+    'pretty_version' => '3.0.9',
+    'version' => '3.0.9.0',
+    'aliases' => 
+    array (
+    ),
+    'reference' => NULL,
+    'name' => 'istvan-ujjmeszaros/bootstrap-duallistbox',
+  ),
+  'versions' => 
+  array (
+    'istvan-ujjmeszaros/bootstrap-duallistbox' => 
+    array (
+      'pretty_version' => '3.0.9',
+      'version' => '3.0.9.0',
+      'aliases' => 
+      array (
+      ),
+      'reference' => NULL,
+    ),
+  ),
+);
